67683ea9d278f6ef2558285c10e89695fb6a4664
yma32071-hue <yma32071@pioneers.edu.ps>
2026-06-08 07:23:45 +0000

    fix: making GRUB cool

---
+++ ZaytoonOS/build/kernel/kernel/printk.d
@@ -0,0 +1,4 @@
+build/kernel/kernel/printk.o: kernel/kernel/printk.c \
+ kernel/kernel/printk.h kernel/drivers/console.h
+kernel/kernel/printk.h:
+kernel/drivers/console.h:
---
+++ ZaytoonOS/build/maincompiler.d
@@ -0,0 +1,3 @@
+build/./maincompiler.o: maincompiler.c kernel/maink.h kernel/types.h
+kernel/maink.h:
+kernel/types.h:

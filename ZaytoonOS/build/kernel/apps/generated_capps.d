886909f0d7d104e3a990088054dda6b601a49c11
yma32071-hue <yma32071@pioneers.edu.ps>
2026-06-14 17:38:35 +0000

    fix: removed output by make

--- ZaytoonOS/build/kernel/apps/generated_capps.d
+++
@@ -1,4 +0,0 @@
-build/kernel/apps/generated_capps.o: kernel/apps/generated_capps.c \
- kernel/apps/capp.h kernel/types.h
-kernel/apps/capp.h:
-kernel/types.h:
--- ZaytoonOS/build/kernel/kernel/task.d
+++
@@ -1,9 +0,0 @@
-build/kernel/kernel/task.o: kernel/kernel/task.c kernel/arch/arch.h \
- kernel/arch/user_entry.h kernel/kernel/context.h kernel/kernel/task.h \
- kernel/kernel/printk.h kernel/mm/mm.h
-kernel/arch/arch.h:
-kernel/arch/user_entry.h:
-kernel/kernel/context.h:
-kernel/kernel/task.h:
-kernel/kernel/printk.h:
-kernel/mm/mm.h:
